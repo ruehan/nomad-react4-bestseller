24ef424ae18b9ab0326e17a2ff779940bc726cbb
vendredl <node.js_@kakao.com>
2024-03-28 23:03:02 +0900

    :speech_balloon: 베스트셀러 목록 Link 구현 테스트

--- src/components/best-seller.tsx
+++ src/components/best-seller.tsx
@@ -1,5 +1,6 @@
 import { API_URL } from "@/app/(home)/page";
 import { BestSellerType } from "@/interface/types";
+import Link from "next/link";
 
 async function getBestSeller() {
 	const response = await fetch(API_URL);
@@ -10,10 +11,12 @@ export default async function BestSeller() {
 	const bestSeller = await getBestSeller();
 	console.log(bestSeller.results);
 	return (
-		<>
+		<div>
 			{bestSeller.results.map((best: BestSellerType) => (
-				<p>{best.list_name}</p>
+				<li>
+					<Link href={`/list/${best.list_name_encoded}`}>{best.list_name}</Link>
+				</li>
 			))}
-		</>
+		</div>
 	);
 }

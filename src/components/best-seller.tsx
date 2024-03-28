652aa96e7b23545c628f6283cb4db54159b6162e
vendredl <node.js_@kakao.com>
2024-03-29 02:18:17 +0900

    :speech_balloon: 기본 구조 구현

--- src/components/best-seller.tsx
+++ src/components/best-seller.tsx
@@ -1,6 +1,7 @@
 import { API_URL } from "@/app/(home)/page";
 import { BestSellerType } from "@/interface/types";
 import Link from "next/link";
+import styles from "../styles/best-seller.module.css";
 
 async function getBestSeller() {
 	const response = await fetch(API_URL);
@@ -9,14 +10,20 @@ async function getBestSeller() {
 
 export default async function BestSeller() {
 	const bestSeller = await getBestSeller();
-	console.log(bestSeller.results);
+	// console.log(bestSeller.results);
 	return (
-		<div>
-			{bestSeller.results.map((best: BestSellerType) => (
-				<li>
-					<Link href={`/list/${best.list_name_encoded}`}>{best.list_name}</Link>
-				</li>
-			))}
-		</div>
+		<>
+			<div className={styles.container}>
+				{bestSeller.results.map((best: BestSellerType) => (
+					<Link
+						key={best.list_name}
+						className={styles.list}
+						href={`/list/${best.list_name_encoded}`}
+					>
+						{best.list_name}
+					</Link>
+				))}
+			</div>
+		</>
 	);
 }
